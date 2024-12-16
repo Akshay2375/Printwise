73d5368dee5ab9b95388c281ae8992dba56fe302
Akshay2375 <mundaleakshay7@gmail.com>
2024-12-16 19:43:16 +0530

    Delete printwise directory

--- printwise/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PrintwiseConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'printwise'
--- printwise/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class MyPrintForm(forms.Form):
-    file = forms.FileField()  # This handles the file input field
-    copies = forms.IntegerField(min_value=1, initial=1)
-    orientation = forms.ChoiceField(choices=[('portrait', 'Portrait'), ('landscape', 'Landscape')])
